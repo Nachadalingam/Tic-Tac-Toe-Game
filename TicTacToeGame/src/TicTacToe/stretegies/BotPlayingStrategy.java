baedfb9acd0328aa06ba4338256fb9a3f5c55a48
Nachadalingam <167499918+Nachadalingam@users.noreply.github.com>
2024-09-14 15:57:04 +0530

    Add files via upload

---
+++ TicTacToeGame/src/TicTacToe/stretegies/BotPlayingStrategy.java
@@ -0,0 +1,9 @@
+
+package TicTacToe.stretegies;
+
+import TicTacToe.models.Board;
+import TicTacToe.models.Move;
+
+public interface BotPlayingStrategy {
+    public Move makeMove(Board board);
+}
---
+++ TicTacToeGame/src/TicTacToe/stretegies/WinningStrategy.java
@@ -0,0 +1,10 @@
+package TicTacToe.stretegies;
+
+import TicTacToe.models.Board;
+import TicTacToe.models.Move;
+import TicTacToe.models.Player;
+
+public interface WinningStrategy {
+    public boolean checkWinner(Move move, Board board);
+    public void handleUndo(Move move , Board board);
+}
